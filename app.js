7678530cc8b5abd27d1ebf6e3ea69a0c693954b8
ABiviano13 <abiviano0@outlook.com>
2023-03-22 16:06:37 +0100

    FEAT axios call

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 const { createApp } = Vue
 
 createApp({
-    
+
+    data() {
+        return {
+            todoList: []
+        }
+    },
+    methods: {
+        fetchList(){
+            axios
+                .get('items.php')
+                .then(res => {
+                    console.log(res.data);
+                }).catch(error => {
+                    console.log(error);
+                })
+        }
+    },
+    mounted() {
+        this.fetchList();
+    }
+
 }).mount('#app')
--- items.php
+++ items.php
@@ -2,6 +2,13 @@
 
 $todo_string = file_get_contents('./items.json');
 
+$todo_list = json_decode($todo_string, true);
+
+$response = [
+  'results' => $todo_list,
+  'success' => true
+];
+
 header('Content-type: application/json');
 
-echo json_encode($todo_string);
+echo json_encode($response);
